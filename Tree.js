a1c9e6614c80b702cd1a2c2203a7ee0022afccfd
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2026-08-14 03:24:00 +0300

    Complete Tree Build Functionality - `buildTree(array)`: passes necessary items to build `tree` & returns `root node` - `buildTreeRecursively(array, start, end)`: builds `tree` recursively - `prettyPrint(node)`: visualizes `tree` in log

--- Tree.js
+++ Tree.js
@@ -1,15 +1,48 @@
+import Node from "./Node.js";
+
 class Tree {
-  #array;
   #root;
   constructor(array) {
-    this.#array = array;
+    // set root node from sorted array
+    this.#root = this.#buildTree(array);
   }
 
   set root(newRoot) {
     this.#root = newRoot;
   }
+  get Root() {
+    return this.#root;
+  }
 
-  #buildTree(array) {}
+  // build tree recursively
+  #buildTreeRecursively(array, start, end) {
+    // base case
+    if (start > end) return null;
+    // create root element
+    const arrayMid = start + Math.floor((end - start) / 2);
+    const node = new Node(array[arrayMid]);
+    // set left and right recursively
+    node.left = this.#buildTreeRecursively(array, start, arrayMid - 1);
+    node.right = this.#buildTreeRecursively(array, arrayMid + 1, end);
+    return node;
+  }
+  // provide sorted array and receive root node
+  #buildTree(array) {
+    // sort array in asc order and filter it from duplicates
+    const sortedArray = array
+      .sort((a, b) => a - b)
+      .filter((num, index) => array.indexOf(num) === index);
+    return this.#buildTreeRecursively(sortedArray, 0, sortedArray.length - 1);
+  }
+  // provided by TOP, visualizes tree in log
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null || node === undefined) {
+      return;
+    }
+    this.prettyPrint(node.Right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.Data}`);
+    this.prettyPrint(node.Left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
 }
 
 export default Tree;
--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import Tree from "./Tree.js";
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array);
+
+tree.prettyPrint(tree.Root);
